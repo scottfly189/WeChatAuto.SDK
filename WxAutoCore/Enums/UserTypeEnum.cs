b70692422d180b7b6aa233dcd0e636ae99397c7e
PZ688 <projectzoomau@gmail.com>
2025-08-12 15:58:39 +0800

    int the project

---
+++ WxAutoCore/Enums/UserTypeEnum.cs
@@ -0,0 +1,11 @@
+namespace WxAutoCore.Enums
+{
+    internal enum UserType
+    {
+        None = 0, //没有用户
+        NoPay = 1,//没有付费用户
+        Pro = 2,//付费用户
+        Ent = 3,//企业用户
+        Test = 4//测试用户
+    }
+}
