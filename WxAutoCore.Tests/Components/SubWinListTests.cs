4986db301f0fa1cb4373320a121af03016363cfb
PZ688 <projectzoomau@gmail.com>
2025-08-19 00:04:04 +0800

    test(子窗口): 测试子窗口列表方法

---
+++ WxAutoCore.Tests/Components/SubWinListTests.cs
@@ -0,0 +1,68 @@
+using Xunit;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core;
+using WxAutoCore.Components;
+using Xunit.Abstractions;
+using WxAutoCore.Services;
+
+namespace WxAutoCore.Tests.Components
+{
+    [Collection("UiTestCollection")]
+    public class SubWinListTests
+    {
+        private readonly string _wxClientName = "Alex Zhao";
+        private readonly ITestOutputHelper _output;
+        private UiTestFixture _globalFixture;
+        public SubWinListTests(ITestOutputHelper output, UiTestFixture globalFixture)
+        {
+            _output = output;
+            _globalFixture = globalFixture;
+            _output.WriteLine("开始测试子窗口的各项功能 =================================================");
+        }
+        [Fact(DisplayName = "测试获取所有子窗口名称")]
+        public async Task TestGetAllSubWinNames()
+        {
+            var framework = _globalFixture.wxFramwork;
+            var client = framework.GetWxClient(_wxClientName);
+            var window = client.WxWindow;
+            window.Conversations.DoubleClickConversation("文件传输助手");
+            var subWinList = window.SubWinList;
+            var subWinNames = subWinList.GetAllSubWinNames();
+            foreach (var subWinName in subWinNames)
+            {
+                _output.WriteLine($"子窗口名称: {subWinName}");
+            }
+            Assert.True(subWinNames.Count > 0);
+            await WxAutomation.Wait(TimeSpan.FromSeconds(5));
+            subWinList.CloseAllSubWins();
+        }
+        [Theory(DisplayName = "测试获取子窗口")]
+        [InlineData("文件传输助手")]
+        public async Task TestGetSubWin(string subWinName)
+        {
+            var framework = _globalFixture.wxFramwork;
+            var client = framework.GetWxClient(_wxClientName);
+            var window = client.WxWindow;
+            window.Conversations.DoubleClickConversation(subWinName);
+            var subWinList = window.SubWinList;
+            var subWin = subWinList.GetSubWin(subWinName);
+            Assert.NotNull(subWin);
+            await WxAutomation.Wait(TimeSpan.FromSeconds(5));
+            subWinList.CloseAllSubWins();
+        }
+        [Theory(DisplayName = "测试判断子窗口是否打开")]
+        [InlineData("文件传输助手")]
+        public async Task TestGetSubWinIsOpen(string subWinName)
+        {
+            var framework = _globalFixture.wxFramwork;
+            var client = framework.GetWxClient(_wxClientName);
+            var window = client.WxWindow;
+            window.Conversations.DoubleClickConversation(subWinName);
+            var subWinList = window.SubWinList;
+            var isOpen = subWinList.GetSubWinIsOpen(subWinName);
+            Assert.True(isOpen);
+            await WxAutomation.Wait(TimeSpan.FromSeconds(5));
+            subWinList.CloseAllSubWins();
+        }
+    }
+}
--- WxAutoCore/Components/SubWinList.cs
+++ WxAutoCore/Components/SubWinList.cs
@@ -15,7 +15,11 @@ namespace WxAutoCore.Components
     {
         private WxWindow _MainWxWindow;
         private Window _MainFlaUIWindow;
-
+        /// <summary>
+        /// 子窗口列表构造函数
+        /// </summary>
+        /// <param name="window">主窗口FlaUI的window</param>
+        /// <param name="wxWindow">主窗口对象<see cref="WxWindow"/></param>
         public SubWinList(Window window, WxWindow wxWindow)
         {
             _MainWxWindow = wxWindow;
@@ -28,11 +32,11 @@ namespace WxAutoCore.Components
         public List<string> GetAllSubWinNames()
         {
             var desktop = _MainFlaUIWindow.Automation.GetDesktop();
-            var subWinRetry = Retry.WhileNull(()=>desktop.FindAllChildren(cf => cf.ByClassName("ChatWnd")
+            var subWinRetry = Retry.WhileNull(() => desktop.FindAllChildren(cf => cf.ByClassName("ChatWnd")
                         .And(cf.ByControlType(ControlType.Window)
                         .And(cf.ByProcessId(_MainFlaUIWindow.Properties.ProcessId)))),
-                        timeout:TimeSpan.FromSeconds(10),
-                        interval:TimeSpan.FromMilliseconds(200));
+                        timeout: TimeSpan.FromSeconds(10),
+                        interval: TimeSpan.FromMilliseconds(200));
             if (subWinRetry.Success)
             {
                 return subWinRetry.Result.ToList().Select(subWin => subWin.Name).ToList();
@@ -69,6 +73,26 @@ namespace WxAutoCore.Components
                 return new WxSubWindow(subWin.Result.AsWindow(), _MainWxWindow);
             }
             return null;
+        }
+
+        /// <summary>
+        /// 关闭所有子窗口
+        /// </summary>
+        public void CloseAllSubWins()
+        {
+            var subWinNames = GetAllSubWinNames();
+            foreach (var subWinName in subWinNames)
+            {
+                GetSubWin(subWinName).Close();
+            }
+        }
+        /// <summary>
+        /// 关闭指定子窗口
+        /// </summary>
+        /// <param name="name">子窗口名称</param>
+        public void CloseSubWin(string name)
+        {
+            GetSubWin(name).Close();
         }
     }
 }
