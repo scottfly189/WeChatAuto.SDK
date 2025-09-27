370d7255882efcaa2a0e6224fbe607d01ed0967f
PZ688 <projectzoomau@gmail.com>
2025-09-28 03:29:18 +0800

    refactor: 开始制作消息监听

--- WeAutoCore/Components/WeChatMainWindow.cs
+++ WeAutoCore/Components/WeChatMainWindow.cs
@@ -662,10 +662,26 @@ namespace WxAutoCore.Components
         #endregion
 
         #region 监听消息
-        public void AddListener(string nickName, Action<MessageBubble, List<MessageBubble>, Planner> callBack)
+        /// <summary>
+        /// 添加消息监听，用户需要提供一个回调函数，当有消息时，会调用回调函数
+        /// callBack回调函数参数：
+        /// 1.新消息气泡<see cref="MessageBubble"/>
+        /// 2.包含新消息气泡的列表<see cref="List{MessageBubble}"/>，适用于给LLM大模型提供上下文
+        /// 3.发送者<see cref="Sender"/>，适用于本子窗口操作，如发送消息、发送文件、发送表情等
+        /// 4.当前微信窗口对象<see cref="WeChatMainWindow"/>，适用于全部操作，如给指定好友发送消息、发送文件、发送表情等
+        /// 5.计划器<see cref="Planner"/>,适用于设定计划任务的场合
+        /// </summary>
+        /// <param name="nickName">好友名称</param>
+        /// <param name="callBack">回调函数</param>
+        /// <param name="monitor">是否启用子窗口监听，如果子窗口被误关，监听器会自动重新打开子窗口</param>
+        public void AddListener(string nickName, Action<MessageBubble, List<MessageBubble>, Sender, WeChatMainWindow, Planner> callBack, bool monitor = true)
         {
-
+            
         }
+        /// <summary>
+        /// 移除监听消息
+        /// </summary>
+        /// <param name="nickName">好友名称</param>
         public void RemoveListener(string nickName)
         {
 
