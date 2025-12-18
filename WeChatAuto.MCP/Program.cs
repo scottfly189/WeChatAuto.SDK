4e0cf17138f35f90456975e8af1bc06a66dd38f1
PZ688 <projectzoomau@gmail.com>
2025-12-18 18:05:03 +0800

    refactor:放弃将mcp server放在nuget上，因为不支持.net10-windows框架

--- WeChatAuto.MCP/Program.cs
+++ WeChatAuto.MCP/Program.cs
@@ -1,47 +1,61 @@
-using System.Diagnostics;
+﻿using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using ModelContextProtocol;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using WeChatAuto.MCP.Utils;
 using WeChatAuto.Services;
+using WeChatAuto.Components;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Text.Json;
+using WeChatAuto.MCP.Tools;
+using WeChatAuto.MCP.Utils;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddLogging(logging =>
+namespace WeChatAuto.MCP;
+
+public static class Program
 {
-    logging.ClearProviders();
-    logging.AddConsole(options =>
+    public static async Task Main(string[] args)
     {
-        options.LogToStandardErrorThreshold = LogLevel.Trace;
-    });
-});
-WeAutomation.Initialize(builder.Services, options =>
-{
-    options.DebugMode = false;  //测试环境可以开启DebugMode
-    options.EnableMouseKeyboardSimulator = false;
-    options.EnableRecordVideo = false;
-}).AddMcpServer()
-    .WithStdioServerTransport()
-    .WithToolsFromAssembly()
-    .WithPromptsFromAssembly();
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole(options =>
+            {
+                options.LogToStandardErrorThreshold = LogLevel.Trace;
+            });
+        });
+        //注册MCP服务，使用标准输入输出传输，从程序集加载工具、提示和资源
+        WeAutomation.Initialize(builder.Services, options =>
+        {
+            options.DebugMode = true;
+            options.EnableMouseKeyboardSimulator = false;
+            options.EnableRecordVideo = true;
+        }).AddMcpServer()
+            .WithStdioServerTransport()
+            .WithToolsFromAssembly()
+            .WithPromptsFromAssembly();
 
-builder.Services.AddSingleton<WeChatClientService>();
+        builder.Services.AddSingleton<WeChatClientService>();
 
-Alert.Show($"当前进程ID: {Process.GetCurrentProcess().Id}");
+        Alert.Show($"当前进程ID: {Process.GetCurrentProcess().Id}");
 
-var host = builder.Build();
+        var host = builder.Build();
 
-try
-{
-    //运行MCP服务
-    await host.RunAsync();
-}
-catch (Exception ex)
-{
-    // 记录异常（如果需要）
-    Alert.Show($"MCP服务运行异常: {ex.Message}");
-    throw;
-}
-finally
-{
-    Alert.Show("MCP服务已停止");
+        try
+        {
+            //运行MCP服务
+            await host.RunAsync();
+        }
+        catch (Exception ex)
+        {
+            // 记录异常（如果需要）
+            Alert.Show($"MCP服务运行异常: {ex.Message}");
+            throw;
+        }
+        finally
+        {
+            Alert.Show("MCP服务已停止");
+        }
+    }
 }
