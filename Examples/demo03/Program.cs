abfeae1082b6af59613c2a07ffe3ef5a4347c265
alex zhao <alexzhao189@gmail.com>
2026-02-08 13:11:51 +0800

    refactor: 增加demo03,在群聊里@好友的演示

--- Examples/demo03/Program.cs
+++ Examples/demo03/Program.cs
@@ -1,4 +1,5 @@
-﻿/***
+﻿
+/***
  * 示例三 - 消息管理演示 - 群中发送消息，并@好友
  */
 
@@ -19,3 +20,4 @@ await client.SendWho("AI.Net", "你好，世界！");
 await client.SendWho("测试11", "你好，世界！", "123321");
 //发送消息给群聊"测试11"，并好友:@123321,@Alex
 await client.SendWho("测试11","你好，世界！",new string[]{"123321","Alex"});
+
