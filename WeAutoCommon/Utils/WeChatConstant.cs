e435b0bee5e7ec33d693b4f278aa70d09aba72f6
alex zhao <alexzhao189@gmail.com>
2026-04-29 09:18:57 +0800

    refactor: 添加对2019 server 系统的微信支持，ver=10.0.17763.973

--- WeAutoCommon/Utils/WeChatConstant.cs
+++ WeAutoCommon/Utils/WeChatConstant.cs
@@ -2,7 +2,7 @@ namespace WeAutoCommon.Utils
 {
     public class WeChatConstant
     {
-        public static readonly string WECHAT_SYSTEM_NAME = WeChatTextIdentifiers.GetText(WeChatTextIdentifiers.WECHAT_SYSTEM, "微信");
+        public static  string WECHAT_SYSTEM_NAME = WeChatTextIdentifiers.GetText(WeChatTextIdentifiers.WECHAT_SYSTEM, "微信");
         public static readonly string WECHAT_SYSTEM_TASKBAR = WeChatTextIdentifiers.GetText(WeChatTextIdentifiers.WECHAT_SYSTEM, "任务栏");
         public static readonly string WECHAT_SYSTEM_NOTIFY_ICON = WeChatTextIdentifiers.GetText(WeChatTextIdentifiers.WECHAT_SYSTEM, "用户提示通知区域");
         public static readonly string WECHAT_SYSTEM_MAX_BUTTON = WeChatTextIdentifiers.GetText(WeChatTextIdentifiers.WECHAT_CHAT_BOX, "最大化");
