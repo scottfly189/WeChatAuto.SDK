fbbeb46d4264f932c62f79d2accdae6beae02a32
alex zhao <alexzhao189@gmail.com>
2026-09-07 16:49:14 +0800

    fixed: 消息窗口 的复制 按钮由于UI Tree的变化导致的错误

--- WeChatAuto4_X/WeChatAuto/Utils/UITreeGlobal.cs
+++ WeChatAuto4_X/WeChatAuto/Utils/UITreeGlobal.cs
@@ -8,6 +8,8 @@ namespace WeChatAuto.Utils
     /// </summary>
     public static class UITreeGlobal
     {
+        //消息列表的根listbox
         public static string MessageRootPath = "/Group/Custom/Group/Group/Group/Custom/Custom/Custom/Group/Custom/Custom/Group/Custom/Group/Group/List[@Name='消息'][@AutomationId='chat_message_list'][@ClassName='mmui::RecyclerListView'] | /Group/Custom/Group/Group/Group/Custom/Custom/Custom/Group/Custom/Custom/Group/Custom/Group/List[@Name='消息'][@AutomationId='chat_message_list'][@ClassName='mmui::RecyclerListView'] | /Group/Group/Group/Custom/Group/Group/List[@Name='消息'][@AutomationId='chat_message_list'][@ClassName='mmui::RecyclerListView'] | /Group/Group/Group/Custom/Group/List[@Name='消息'][@AutomationId='chat_message_list'][@ClassName='mmui::RecyclerListView']";
+        public static string MessagePopupMenu_Copy = "/Window[@Name='Weixin']/MenuItem[@Name='复制'][@ClassName='mmui::XMenuView'] | /Window[@Name='Weixin']/MenuItem[@Name='复制'][@ClassName='mmui::XMenu']";
     }
 }
