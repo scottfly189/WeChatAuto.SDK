32428ca734bba8a7dddf97c223027d2062d2f69a
alex zhao <alexzhao189@gmail.com>
2026-09-12 21:07:34 +0800

    refactor: 去掉common的sqlsugar

--- WeChatAuto4_X/WeChatAuto/Options/PullMessagesOptions.cs
+++ WeChatAuto4_X/WeChatAuto/Options/PullMessagesOptions.cs
@@ -94,7 +94,7 @@ namespace WeChatAuto.Options
         public string ConnectionString { get; set; } = "Data Source=wechat.db";
 
         /// <summary>
-        /// 默认获取的最新消息写入上面设置的数据库，但是考虑到一些不想直接写数据库，如：调用webapi写服务器数据库，可以在此设置回调,
+        /// 默认获取的最新消息写入上面设置的数据库，但是考虑到一些不想直接写数据库，如：调用webapi写服务器数据库，可以在此设置回调函数，在回调函数中调用webapi,
         /// 注意：如果不想写数据库，需要将DbType设置为DbType.Custom,否则此方法不起效果.
         /// 传入的参数: SDK传入最新获取的消息列表,详情请参考<seealso cref="SimpleMessageBubble"/>
         /// 返回： 写入为真还是假.
